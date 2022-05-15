c27d782f8af4021d2db334c1bf067f26ef51e979
LIKKASID <92563951+LIKKASID@users.noreply.github.com>
2022-05-15 13:45:54 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <router-link class="link" :to="{ path: '/Profile' }">Profile</router-link>
+    <router-link class="link" :to="{ path: '/Contact' }">Contact</router-link>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  margin: 0px;
+}
+.link {
+  display: inline-block;
+  padding: 10px;
+  text-decoration: none;
+  color: inherit;
+  font-size: 18px;
+}
+.router-link-exact-active {
+  color: blue;
+}
+body {
+  background-color: #90969d;
+}
+</style>
---
+++ src/components/Contact.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="t1">
+    <h1>MY CONTACT</h1>
+    <div class="t3">
+      <h3><li>E mail : peerapol0990@gmail.com</li></h3>
+      <h3><li>Phone : 0943575729</li></h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+h1 {
+  font-size: 40px;
+}
+.t1 {
+  text-align: center;
+}
+.t3 {
+  font-size: 16px;
+  text-align: left;
+  text-indent: 10em;
+}
+</style>
